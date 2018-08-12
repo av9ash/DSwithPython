1a001c32a7206ab6ff0de2bb05da789429ad2626
Avinash <avinash2k9@yahoo.com.au>
2018-08-12 01:37:00 -0700

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,42 @@
+class Queue(object):
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self,data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        x = self.queue[0]
+        del self.queue[0]
+        return x
+
+    def isEmpty(self):
+        return self.queue ==[]
+
+    def peek(self):
+        if not self.isEmpty():
+            return self.queue[0]
+
+    def length(self):
+        return len(self.queue)
+
+    def printQ(self):
+        print(list(self.queue))
+
+def main():
+
+    q = Queue()
+
+    for i in range(0, 10):
+        q.enqueue(i)
+
+    q.printQ()
+
+    print(q.isEmpty())
+    print(q.length())
+    print(q.peek())
+    print(q.dequeue())
+    print(q.peek())
+
+if __name__=='__main__':
+    main()
